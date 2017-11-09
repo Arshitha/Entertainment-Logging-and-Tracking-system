f2a4bfb59bbaec5ea70add508d7b8f7eb432af16
Xintong Hao <31745643+XintongHao@users.noreply.github.com>
2017-11-08 22:49:32 -0500

    Add files via upload

--- learnMod/April/Veoshare/public/app.js
+++ learnMod/April/Veoshare/public/app.js
@@ -1,58 +1,61 @@
 alert("hello");
-(function(){
-	  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyALxLWKCNLarn6KSUN22d3YN_8nJ7HXv1U",
-    authDomain: "psychic-cycling-185003.firebaseapp.com",
-    databaseURL: "https://psychic-cycling-185003.firebaseio.com",
-    projectId: "psychic-cycling-185003",
-    storageBucket: "",
-    messagingSenderId: "437139562140"
-  };
-  firebase.initializeApp(config);
 
-  //Get elements
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-  const btnLogout = document.getElementById('btnLogout');
-  //Add login event
-  btnLogin.addEventListener('click', e =>{
-  	//Get email and password
-  	const email = txtEmail.value;
-  	const pass = txtPassword.value;
-  	const auth = firebase.auth();
-  	//Sign in 
-  	const promise = auth.signInWithEmailAndPassword(email,pass);
-  	promise.catch(e => console.log(e.message));
-  });
-  //Add signup event
-  btnSignUp.addEventListener('click', e =>{
-  	//Get email and password
-  	//TODO: check 4 real emailz
-  	const email = txtEmail.value;
-  	const pass = txtPassword.value;
-  	const auth = firebase.auth();
-  	//Sign in 
-  	const promise = auth.createUserWithEmailAndPassword(email,pass);
-  	promise.catch(e => console.log(e.message));  	
-  });
+var provider = new firebase.auth.GoogleAuthProvider();
+var user;
+// Get a reference to the database service
+var database = firebase.database();
 
-  btnLogout.addEventListener('click', e =>{
-  	firebase.auth().signOut();
-  });
-
-  //Add a realtime listener
-  firebase.auth().onAuthStateChanged(firebaseUser =>{
-  	if(firebaseUser){
-  		console.log(firebaseUser);
-  		btnLogout.classList.remove('hide');
-  	}else{
-  		console.log('not logged in');
-  		btnLogout.classList.add('hide');
-  	}
- 
-  });
-
+$( document ).ready(function(){
+	$("#welcome").hide();
 });
+
+function signIn(){
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  console.log(user.displayName);
+	  console.log(user);
+	  user = result.user;
+	  showWelcomeContainer();
+
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+
+}
+
+function showWelcomeContainer(){
+	$("#login").hide();
+	$("#welcome").show();
+	$("#welcomeText").html("Hello, " + user.displayName);
+};
+
+
+$(".dropdown").on("hide.bs.dropdown", function(event){
+    var text = $(event.relatedTarget).text(); // Get the text of the element
+  	firebase.database().ref('users/' + user.uid).set({
+    	username: user.displayName,
+    	email: user.email,
+    	movie_type : text
+ 	});    
+   
+});
+
+
+// function writeUserData(userId, name, email, imageUrl) {
+//   firebase.database().ref('users/' + userId).set({
+//     username: user.displayName,
+//     email: user.email,
+// //    profile_picture : imageUrl
+//   });
+// }
