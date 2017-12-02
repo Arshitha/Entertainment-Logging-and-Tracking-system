cf55ea3be12375ba6cf89c093a88f17762188366
Arshitha <arsh2794@gmail.com>
2017-12-01 21:08:04 -0500

    trial api call results for trailers integrated with OMDb api

--- LearningModules/Arshitha/Trailers/app.js
+++ LearningModules/Arshitha/Trailers/app.js
@@ -3,6 +3,11 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+
+var vidLink = new Array()
+var link = " ";
+
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -14,31 +19,43 @@ app.get("/results", function(req, res){
     var query = req.query.keyword;
     console.log(query)
     
-    
     var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
     
     request(url,function(error, response, body){
         if(!error && response.statusCode == 200){
             var parsedData = JSON.parse(body);
-            //res.render("results", {data: parsedData});
+            
             
             parsedData["Search"].forEach(function(movie){ 
-    
-                
                 var movieID = movie.imdbID;
-                console.log(movieID);
+                //console.log(movieID);
                 console.log(movie.Title)
                 var urlOne = "http://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=fc2ec17d17b2ffeee2d9053a7ac02965&append_to_response=videos";
-                console.log(urlOne)
+                //console.log(urlOne)
                 request(urlOne,function(error, response, body){
                     if(!error && response.statusCode == 200){
                         var parsedDataOne = JSON.parse(body);
-                        console.log(parsedDataOne)
-                        res.render("trialers", {data: parsedDataOne});
+                        //var key = parsedDataOne["results"][0]["key"]
+                        //var link = "https://www.youtube.com/watch?v=" + key;
+                        
+                    
+                        //parsedDataOne["Search"][0]
+                        /*parsedDataOne["results"].forEach(function(searchResult){
+                            var key = searchResult.key;
+                            link = "https://www.youtube.com/watch?v=" + key;
+                            //vidLink.push(link)
+                            
+                            
+                            //res.send(link)
+                        });*/
+                        console.log(vidLink)
                     }
-                 });
+                });
+                vidLink.push(link)
                     
             });
+            
+            res.render("results", {data: parsedData});
             
         }
     })
