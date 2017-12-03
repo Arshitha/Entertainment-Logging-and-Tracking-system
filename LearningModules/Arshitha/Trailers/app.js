90b4845f2b32c1dcbb51add0c610a20cebbeb8a6
Arshitha <arsh2794@gmail.com>
2017-12-03 18:19:46 -0500

    basic functionality of info view

--- LearningModules/Arshitha/Trailers/app.js
+++ LearningModules/Arshitha/Trailers/app.js
@@ -3,11 +3,6 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-
-var vidLink = new Array()
-var link = " ";
-
-
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -24,63 +19,57 @@ app.get("/results", function(req, res){
     request(url,function(error, response, body){
         if(!error && response.statusCode == 200){
             var parsedData = JSON.parse(body);
-            
-            
-            parsedData["Search"].forEach(function(movie){ 
-                var movieID = movie.imdbID;
-                //console.log(movieID);
-                console.log(movie.Title)
-                var urlOne = "http://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=fc2ec17d17b2ffeee2d9053a7ac02965&append_to_response=videos";
-                //console.log(urlOne)
-                request(urlOne,function(error, response, body){
-                    if(!error && response.statusCode == 200){
-                        var parsedDataOne = JSON.parse(body);
-                        //var key = parsedDataOne["results"][0]["key"]
-                        //var link = "https://www.youtube.com/watch?v=" + key;
-                        
-                    
-                        //parsedDataOne["Search"][0]
-                        /*parsedDataOne["results"].forEach(function(searchResult){
-                            var key = searchResult.key;
-                            link = "https://www.youtube.com/watch?v=" + key;
-                            //vidLink.push(link)
-                            
-                            
-                            //res.send(link)
-                        });*/
-                        console.log(vidLink)
-                    }
-                });
-                vidLink.push(link)
-                    
-            });
-            
-            res.render("results", {data: parsedData});
-            
+            if (parsedData["Search"]){
+                res.render("results", {data: parsedData});
+            }
+            else{
+                res.send("Data unavailable")
+            }
         }
     })
 });
+
+app.get("/info",function(req, res){
+    var query = req.query.movieID;
+    var addData
     
 
-/*app.get("/trailers", function(req, res){
-    var query = req.query.searchTerm;
-    
-    console.log(query);
-    var movieID = "tt1201607";
-    var url = "http://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=fc2ec17d17b2ffeee2d9053a7ac02965&append_to_response=videos";
-    
-    request(url,function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var parsedData = JSON.parse(body);
-            console.log(parsedData)
-            res.render("trialers", {data: parsedData});
+    var urlOne = "http://www.omdbapi.com/?apikey=thewdb&i=" + query;
+            
+    request(urlOne, function(err, res, bod){
+        if(!err && res.statusCode == 200){
+            var parsedDataOne = JSON.parse(bod);
+        
+         addData = {Title: parsedDataOne.Title, Released: parsedDataOne.Released, Year: parsedDataOne.Year, Plot: parsedDataOne.Plot, Actors: parsedDataOne.Actors, Ratings: parsedDataOne.Ratings }
+            
         }
-    });
+         
+        
+    } );
     
-})*/
+    //console.log(addData)
+    //var moviePlot = req.query.moviePlot;
+    //console.log(req.query)
+    var url = "http://api.themoviedb.org/3/movie/" + query + "/videos?api_key=fc2ec17d17b2ffeee2d9053a7ac02965&append_to_response=videos";
+    //console.log(url)
     
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            
+            var parsedData = JSON.parse(body);
+            //console.log(parsedData)
+            if (parsedData["results"]){
+                res.render("info", {data: parsedData, query: addData});
+                console.log({data: parsedData, query: addData})
+            }
+            else{
+                res.send("Trailer unavailable")
+            }
+            
+        }
+    })
     
-
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Movie app has started")
